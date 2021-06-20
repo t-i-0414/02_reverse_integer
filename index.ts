b81f3e29caaaec066ea04cc938dd2b5c32646147
iwashiro-takuya <gutlo.ol0414@gmail.com>
2021-06-20 17:39:13 +0900

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,1 +1,21 @@
-console.log("hello world");
+const reverse = (x: number): number => {
+  const isNegative = Math.sign(x) === -1;
+
+  const raw = isNegative ? -x : x;
+
+  const arr = `${raw}`.split("").reverse();
+
+  while (arr.length > 1 && arr[0] === "0") {
+    arr.shift();
+  }
+
+  const result = isNegative ? -Number(arr.join("")) : Number(arr.join(""));
+
+  if (Math.abs(result) >= 2 ** 31) {
+    return 0;
+  }
+
+  return result;
+};
+
+export default reverse;
